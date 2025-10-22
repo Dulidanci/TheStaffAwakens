253cdb774c8565e435a96b0665e8bb2933a89a00
Dulidanci <dulacska.daniel@gmail.com>
2025-10-23 00:31:47 +0200

    Display feature values are now loaded from json files

--- src/main/java/net/dulidanci/staffmod/render/item/StaffItemRenderer.java
+++ src/main/java/net/dulidanci/staffmod/render/item/StaffItemRenderer.java
@@ -4,13 +4,15 @@ import net.dulidanci.staffmod.StaffMod;
 import net.dulidanci.staffmod.item.custom.StaffItem;
 import net.dulidanci.staffmod.render.model.core.CoreModel;
 import net.dulidanci.staffmod.render.model.staff.StaffModel;
+import net.dulidanci.staffmod.util.json.ModelTransformationLoader;
 import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry;
 import net.minecraft.client.render.*;
+import net.minecraft.client.render.model.json.ModelTransformation;
 import net.minecraft.client.render.model.json.ModelTransformationMode;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.RotationAxis;
 
 public class StaffItemRenderer implements BuiltinItemRendererRegistry.DynamicItemRenderer {
     private final Identifier displayDirectory = new Identifier(StaffMod.MOD_ID, "display/");
@@ -21,52 +23,19 @@ public class StaffItemRenderer implements BuiltinItemRendererRegistry.DynamicIte
             return;
         }
 
-        boolean isLeftHanded = (mode == ModelTransformationMode.THIRD_PERSON_LEFT_HAND || mode == ModelTransformationMode.FIRST_PERSON_LEFT_HAND);
+        Identifier staffItemId = Registries.ITEM.getId(item.getStaff().getType().getItem());
 
         matrices.push();
 
-        switch (mode) {
-            case THIRD_PERSON_RIGHT_HAND -> {
-                matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(70));
-                matrices.translate(0.45, -0.15, -0.325);
-                matrices.scale(1.75F, 1.75F, 1.75F);
-            }
-            case THIRD_PERSON_LEFT_HAND -> {
-                matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(70));
-                matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(180));
-                matrices.translate(-0.55, -0.15, 0.225);
-                matrices.scale(1.75F, 1.75F, 1.75F);
-            }
-            case FIRST_PERSON_RIGHT_HAND -> {
-                matrices.translate(0.85, -0.5, 0.0);
-                matrices.scale(1.75F, 1.75F, 1.75F);
-            }
-            case FIRST_PERSON_LEFT_HAND -> {
-                matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(180));
-                matrices.translate(0.0, -0.4375, 0.0);
-                matrices.scale(1.75F, 1.75F, 1.75F);
-            }
-            case GROUND -> {
-                matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(90));
-                matrices.translate(0.5, -0.1, -0.5);
-            }
-        }
-
-//            ModelTransformation staffTransformation = ModelTransformationLoader.getOrLoadModelTransformation(new Identifier(
-//                    displayDirectory.getNamespace(), displayDirectory.getPath() + itemId.getPath() + ".json"));
-//            staffTransformation.getTransformation(mode).apply(false, matrices);
+        ModelTransformation staffTransformation = ModelTransformationLoader.getOrLoadModelTransformation(new Identifier(
+                displayDirectory.getNamespace(), displayDirectory.getPath() + staffItemId.getPath() + ".json"));
+        staffTransformation.getTransformation(mode).apply(false, matrices);
 
         StaffModel staffModel = item.getStaff().getModel();
         Identifier STAFF_TEXTURE = staffModel.getTexture();
 
         CoreModel coreModel = item.getCore().getModel(staffModel.getCoreCubeOriginPoint());
         Identifier CORE_TEXTURE = coreModel.getTexture();
-
-
-//            float time = MinecraftClient.getInstance().world.getTime() + MinecraftClient.getInstance().getRenderTickCounter().getTickDelta(false);
-
-        // animáció
-//            staffModel.setAngles(time);
 
         VertexConsumer staffVertices = vertices.getBuffer(staffModel.getLayer(STAFF_TEXTURE));
         staffModel.render(matrices, staffVertices, light, overlay, 1f, 1f, 1f, 1f);
